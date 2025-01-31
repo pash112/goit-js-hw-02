3d01e9b8b0784617e16cadf47132e3853a78d094
Pavlo Markovsky <pash199060@gmail.com>
2025-02-01 00:37:19 +0200

    new

--- js/task-2.js
+++ js/task-2.js
@@ -5,7 +5,6 @@ function formatMessage(message, maxLength) {
         return message.slice(0, maxLength) + '...';
     }
 }
-
 console.log(formatMessage('Curabitur ligula sapien', 16));
 console.log(formatMessage('Curabitur ligula sapien', 23));
 console.log(formatMessage('Vestibulum facilisis purus nec', 20));
--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,12 @@
 function checkForSpam(message) {
     const lowerCaseMessage = message.toLowerCase();
-    return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
-}
 
+    if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
+        return true;
+    } else {
+        return false;
+    }
+}
 console.log(checkForSpam('Latest technology news'));
 console.log(checkForSpam('JavaScript weekly newsletter'));
 console.log(checkForSpam('Get best sale offers now!'));
