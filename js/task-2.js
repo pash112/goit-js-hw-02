8717fd73e1088a1caa01f65e3a8acde1d90f442a
Pavlo Markovsky <pash199060@gmail.com>
2025-02-01 00:16:47 +0200

    new

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,3 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
-console.log(getShippingMessage('Australia', 120, 50));
-console.log(getShippingMessage('France', 150, 20));
-console.log(getShippingMessage('Canada', 200, 30));
 function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
         return message;
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,3 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    return totalWidth;
-}
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
 function checkForSpam(message) {
     const lowerCaseMessage = message.toLowerCase();
     return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
